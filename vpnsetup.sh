b4c0ace72222723749d9ae0ed01602024a7a7e8f
EC2 Default User <ec2-user@ip-172-31-34-201.ec2.internal>
2016-12-30 06:43:53 +0000

    Tested on EC2 instances

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 #install required packages
+echo Install Requried Packages...
 yum update
-yum -y install rng-tools-5-7.12
-yum -y install epel-release
-yum -y install openswan-2.6.37-3.17
-yum -y install xl2tpd-1.3.8-1
-
-#start random number generator
-service rngd start
+yum install -y --enablerepo=epel openswan xl2tpd 
 
 #get local private ip
 PRIVATE_IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
+echo Your local private IP is: $PRIVATE_IP 
 
 #change ipsec.conf
+echo Changing ipsec.conf ...
 IPSEC_CONF='/etc/ipsec.conf'
 echo "
 conn L2TP-PSK
@@ -36,29 +33,49 @@ conn L2TP-PSK
     dpdaction=clear" >> $IPSEC_CONF
 
 #config the secret
-echo enter a shared secrete:
+echo Please enter a shared secret (Remember it, would be used for VPN connection):
 read MY_SECRETE
-IPSEC_SECRETE="my.secrets"
-echo "%any %any : PSK $MY_SECRETE" >> "/etc/ipsec.d/$IPSEC_SECRETE"
+IPSEC_SECRETE="/etc/ipsec.secrets"
+echo "%any %any : PSK \"$MY_SECRETE\"" >> $IPSEC_SECRETE
 
 #enable ip forwarding
+echo Enable IP forwarding...
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
 
 #disable redirects
+echo Disable IMCP redirects... 
 for f in  /proc/sys/net/ipv4/conf/*/accept_redirects; do echo 0 > $f; done
 
 for f in   /proc/sys/net/ipv4/conf/*/send_redirects; do echo 0 > $f; done
 
 sysctl -p
+
+echo Restart IP tables 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 service iptables save
 service iptables restart
 
-init 6
 
-#xl2tpd setup
-#XL2TPD_CONF=/etc/xl2tpd/xl2tpd.conf
-#sed -ir "s/local ip [0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/local ip $PRIVATE_IP/" $XL2TPD_CONF
-#sed -ir "s/require chap=no/require chap=yes/" $XL2TPD_CONF
-#sed -ir "s/require chap=no/require chap=yes/" $XL2TPD_CONF
+#xl2tpd conf setup
+echo set xl2tp configuration
+XL2TPD_CONF=/etc/xl2tpd/xl2tpd.conf
+SERVER_NAME=MY_VPN_SERVER
+sed -ir "s/require chap = no/require chap = yes/" $XL2TPD_CONF
+sed -ir "s/name = .*/name = $SERVER_NAME/" $XL2TPD_CONF
 
+#xl2tpd password setup
+echo Please enter an account name for your VPN connection (Remember it, would be used for VPN connection):
+read CLIENT_NAME
+echo Please enter an password for your VPN connection (Remember it, would be used for VPN connection):
+read PASSWD
+
+PASSWD_FILE=/etc/ppp/chap-secrets
+echo "$CLIENT_NAME $SERVER_NAME $PASSWD *" >> $PASSWD_FILE
+
+#restarting services
+echo Restarting ipsec services...
+service ipsec restart
+echo Restarting xl2tpd services...
+service xl2tpd restart
+
+echo All done. Enjoy =D
